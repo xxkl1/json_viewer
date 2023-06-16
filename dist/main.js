16275a4693a9dac4b439c2472b4907e1baf79c3e
xxkl1 <sanyuelihuaxue@gmail.com>
2023-06-17 00:30:44 +0800

    feat: 配置开发模式

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+import { log, } from './utils.js';
+var __main = function () {
+    log('ok');
+};
+__main();
---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import {
+    log,
+} from './utils.js'
+
+const __main = function () {
+    log('ok')
+}
+
+__main()
